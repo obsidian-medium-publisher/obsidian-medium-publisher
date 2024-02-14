c3235e733258cb979e7da273b0198e9f77c294ea
Overraddit <kenwooshim@gmail.com>
2024-02-14 20:13:10 +0900

    Feat: insert userId field to MyPluginSettings

--- modules/setting/setting.values.ts
+++ modules/setting/setting.values.ts
@@ -1,9 +1,11 @@
 export interface MyPluginSettings {
 	mySetting: string;
 	viewGuide: boolean;
+	userId: string;
 }
 
 export const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default',
-	viewGuide: true
+	viewGuide: true,
+	userId: '',
 }
