b2c7cb7160dd7243fcc9cdea7c28fa9541d8622c
koreanddinghwan <myunghwan0421@gmail.com>
2024-02-16 01:27:45 +0900

    refactor: getUserId 정리

--- modules/api/medium/handler.ts
+++ modules/api/medium/handler.ts
@@ -8,6 +8,10 @@ export class MediumApiHandler {
 	}
 
 	async getUserId() {
+		if (this.app.settingModule.settings.userId !== "") {
+			return this.app.settingModule.settings.userId;
+		}
+
 		const url = "https://api.medium.com/v1/me";
 		const token = this.app.settingModule.settings.mySetting;
 		const requestUrlParam: RequestUrlParam = {
@@ -23,5 +27,26 @@ export class MediumApiHandler {
 		const id = result?.json?.data?.id;
 		if (id === undefined || id === "") throw Error("userId is undefined");
 		return id;
+	}
+
+	async postActiveMd(userId: string, title: string, content: string) {
+		const token = this.app.settingModule.settings.mySetting;
+		const url = `https://api.medium.com/v1/users/${userId}/posts`;
+
+		const result = await requestUrl({
+			method: "POST",
+			headers: {
+				Authorization: `Bearer ${token}`,
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			url,
+			body: JSON.stringify({
+				title,
+				contentFormat: "markdown",
+				content,
+			}),
+		});
+		console.log(result);
 	}
 }
